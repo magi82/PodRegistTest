29f8cddf6ed29be85e6c31bb7f90ba403f1a8e73
byungkook hwang <bkhwang82@gmail.com>
2017-05-23 12:15:36 +0900

    no message

---
+++ PodRegistTest/PodRegistTest.h
@@ -0,0 +1,19 @@
+//
+//  PodRegistTest.h
+//  PodRegistTest
+//
+//  Created by 황병국 on 23/05/2017.
+//  Copyright © 2017 magi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PodRegistTest.
+FOUNDATION_EXPORT double PodRegistTestVersionNumber;
+
+//! Project version string for PodRegistTest.
+FOUNDATION_EXPORT const unsigned char PodRegistTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PodRegistTest/PublicHeader.h>
+
+
